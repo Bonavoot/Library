4d6aa1a49cf013343c92e2393bcc166c939df664
Joey Bonventre <jbonventre95@gmail.com>
2022-03-03 15:05:19 -0500

    Refactored with classes

--- scripts.js
+++ scripts.js
@@ -1,94 +1,88 @@
+let myLibrary = [];
+
+
 class Book {
-    constructor(titles, author, page, read) {
+    constructor(titles, author, page) {
         this.titles = titles;
         this.author = author;
         this.page = page;
-        this.read = read;
+        this.read = false;
     }
-    
-}
-
-let myLibrary = [];
-let newBook;
-
-
-function addBooktoLibrary() {
-    newBook = new Book(titles, author, page, read);
-    newBook.titles = titles.value;
-    newBook.author = author.value;
-    newBook.page = page.value;
-    newBook.read = read.value;
-    myLibrary.push(newBook);
-    
-    createBook(newBook)
-}
-
-function createBook(item) {
-    
-    let dom = document.createElement('div');
-    let container = document.querySelector('.bookDisplay');
-    let newContainer = document.createElement('div');
-    let nTitle = document.createElement('div');
-    let nAuthor = document.createElement('div');
-    let nPages = document.createElement('div');
-    let remove = document.createElement('button');
-    let read = document.createElement('input');
-    let answer = document.createElement('div');
-
-    dom.classList.add(myLibrary.length)
-    container.appendChild(dom);
-
-    newContainer.classList.add('container')
-    dom.appendChild(newContainer);
-
-    remove.classList.add('remove');
-    remove.textContent = 'X';
-    newContainer.appendChild(remove)
-
-    nTitle.classList.add('title')
-    newContainer.appendChild(nTitle);
-
-    nAuthor.classList.add('author')
-    newContainer.appendChild(nAuthor);
-
-    nPages.classList.add('pages');
-    newContainer.appendChild(nPages);
-
-    read.classList.add('read');
-    read.setAttribute('type', 'checkbox')
-    newContainer.appendChild(read);
-
-    answer.classList.add('answer');
-    answer.textContent = 'Not Read';
-    newContainer.appendChild(answer);
-
-    // Remove button
-    remove.addEventListener('click', () => {
-        myLibrary.pop(myLibrary[dom.className]);
-        dom.remove(dom.className);
-    })
-
-    // read/not read button needs work
-    read.addEventListener('click', () => {
-
-        if (answer.textContent == 'Not Read') {
-            answer.textContent = 'Read'
-        }
-        else {
-            answer.textContent = 'Not Read';
-        }
-    });
-
-    for (let i = 0; i < myLibrary.length; i++) {
-
-        nTitle.textContent = myLibrary[i].titles;
-        nAuthor.textContent = 'By ' + myLibrary[i].author;
-        nPages.textContent = myLibrary[i].page + ' Pgs';
         
-        
+        // Takes data from form values, stores book in myLibrary array
+        createBook(item) {
+            
+        let newBook = new Book(titles, author, page);
+        newBook.titles = titles;
+        newBook.author = author;
+        newBook.page = page;
+        myLibrary.push(item);
+                
+        let dom = document.createElement('div');
+        let container = document.querySelector('.bookDisplay');
+        let newContainer = document.createElement('div');
+        let nTitle = document.createElement('div');
+        let nAuthor = document.createElement('div');
+        let nPages = document.createElement('div');
+        let remove = document.createElement('button');
+        let read = document.createElement('input');
+        let answer = document.createElement('div');
+    
+        dom.classList.add(myLibrary.length)
+        container.appendChild(dom);
+    
+        newContainer.classList.add('container')
+        dom.appendChild(newContainer);
+    
+        remove.classList.add('remove');
+        remove.textContent = 'X';
+        newContainer.appendChild(remove)
+    
+        nTitle.classList.add('title')
+        newContainer.appendChild(nTitle);
+    
+        nAuthor.classList.add('author')
+        newContainer.appendChild(nAuthor);
+    
+        nPages.classList.add('pages');
+        newContainer.appendChild(nPages);
+    
+        read.classList.add('read');
+        read.setAttribute('type', 'checkbox')
+        newContainer.appendChild(read);
+    
+        answer.classList.add('answer');
+        answer.textContent = 'Not Read';
+        newContainer.appendChild(answer);
+    
+        // Remove button
+        remove.addEventListener('click', () => {
+            myLibrary.pop(myLibrary[dom.className]);
+            dom.remove(dom.className);
+        })
+    
+        // read/not read button needs work
+        read.addEventListener('click', () => {
+    
+            if (answer.textContent == 'Not Read') {
+                answer.textContent = 'Read'
+            }
+            else {
+                answer.textContent = 'Not Read';
+            }
+        });
+    
+        for (let i = 0; i < myLibrary.length; i++) {
+    
+            nTitle.textContent = myLibrary[i].titles;
+            nAuthor.textContent = 'By ' + myLibrary[i].author;
+            nPages.textContent = myLibrary[i].page + ' Pgs';
+            
+            
+        }
     }
-
 }
+
 
 
 let titles = document.getElementById('titles');
@@ -108,8 +102,10 @@ document.getElementById('addBook').onclick = function () {
 
 document.getElementById('submit').onclick = function (e) {
     e.preventDefault();
+    let newBook = new Book(titles.value, author.value, page.value);
+    newBook.createBook(newBook);
     document.getElementById('form').style.display = 'none'
-    addBooktoLibrary();
+    
 }
 
 
